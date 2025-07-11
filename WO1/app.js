fd9405c93c744cd813f7a9a09156cc7c259b732c
rehubdeveloper <rehubdevelopers@gmail.com>
2025-07-11 10:26:57 +0100

    fixed WAVE error

--- WO1/app.js
+++ WO1/app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const expressLayouts = require('express-ejs-layouts');
 
 // Set EJS as templating engine
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.set('layout', 'layout');
+
+// Use express-ejs-layouts
+app.use(expressLayouts);
 
 // Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
